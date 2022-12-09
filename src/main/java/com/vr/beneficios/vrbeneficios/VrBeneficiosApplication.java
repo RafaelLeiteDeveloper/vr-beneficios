cd7df154fb9df95043624dcf7f933e9584e1ab28
Rafael Leite <rafael.leite@solution4you.com.br>
2022-12-09 16:29:06 -0300

    :tada: Commit inicial

---
+++ src/main/java/com/vr/beneficios/vrbeneficios/VrBeneficiosApplication.java
@@ -0,0 +1,13 @@
+package com.vr.beneficios.vrbeneficios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VrBeneficiosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VrBeneficiosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vr/beneficios/vrbeneficios/VrBeneficiosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vr.beneficios.vrbeneficios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VrBeneficiosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
